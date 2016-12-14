733ab4780a2f417886b994b6742b309258773667
meganetaaan <ishikawa.s.1027@gmail.com>
2016-12-15 01:16:02 +0900

    take a photo via GET

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const Express = require('express')
+const RaspiCam = require('raspicam')
 
 const app = Express()
 const port = 8000
 
-app.get('/', (res, req) => {
-  req.json({path: '/hoge/hoge'})
+app.get('/', (req, res) => {
+  const path = Date.now().toString()
+  const cam = new RaspiCam({
+    mode: 'photo',
+    output: `img/${path}.jpg`
+  })
+  cam.start()
+  cam.on('read', (err, timestamp, filename) => {
+    cam.stop()
+    if (err != null) {
+      res.status(500).json(err)
+    } else {
+      res.json({filename})
+    }
+  })
 })
 
 app.listen(port, () => {
