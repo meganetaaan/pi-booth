4248749c822d59870cebbb87cce536e5935657d8
meganetaaan <ishikawa.s.1027@gmail.com>
2016-12-16 01:23:38 +0900

    sequencial filename

--- index.js
+++ index.js
@@ -4,15 +4,31 @@ const RaspiCam = require('raspicam')
 const app = Express()
 const port = 3000
 
+function to2digits (num) {
+  return ('0' + num).slice(-2)
+}
+
+var seq = 0;
+function getSeq () {
+  return seq++
+}
 app.get('/', (req, res) => {
-  const path = Date.now().toString()
+  const date = new Date()
+  const yyyy = date.getFullYear()
+  const MM = to2digits(date.getMonth() + 1)
+  const dd = to2digits(date.getDate())
+  const seq = to2digits(getSeq())
+  const path = `${yyyy + MM + dd}_${seq}.jpg`
   const cam = new RaspiCam({
     mode: 'photo',
-    output: `img/${path}.jpg`,
+    output: `img/${path}`,
     rot: 180
   })
   cam.start()
   cam.on('read', (err, timestamp, filename) => {
+    if (err != null) {
+      res.status(503).send()
+    }
     if (filename[filename.length - 1] !== '~') {
       cam.stop()
       res.json({filename})
